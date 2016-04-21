0e4d304bdc2643ad08f93084e108876fc4f5ccbd
redf1sh <unreal.770@gmail.com>
2016-04-21 22:26:10 +0300

    Added SFML and Box2D

---
+++ Worms_SFML/Worms_SFML/includes.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <iostream>
+
+#include <Box2D\Box2D.h>
+
+#include <SFML\Graphics.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Audio.hpp>
+
+
+#include "main.hpp"
+
+#endif
