c622aeafa65e27812d1ec35ba89cc6c4b2073fde
redf1sh <unreal.770@gmail.com>
2016-04-25 00:17:07 +0300

    Доделано доп. меню

--- Worms_SFML/Worms_SFML/render.hpp
+++ Worms_SFML/Worms_SFML/render.hpp
@@ -5,11 +5,15 @@
 extern bool eventMenuRender;
 //Вывод меню настроек
 extern bool eventGameSettingsRender;
-//Номер текстуры для отображения
+//Номер выбранной текстуры для генерации карты
 extern int numOfTexture;
+//Номер выбранного цвета
+extern int colorChooseNumber;
 //Задний фон меню
 extern sf::Texture backgroundTexture;
 extern sf::Sprite backgroundSprite;
+//Определение цветов
+extern sf::Color red, blue, white, red, green, yellow, orange;
 
 //Рендер меню
 void renderMenu(bool render);
